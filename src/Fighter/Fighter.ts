34bde1db07fddfbb8f8091f960e211a7eab65d5c
Crisna Bezerra <crisnabto@gmail.com>
2022-11-18 19:26:18 -0300

    Refatorando Fighter e conclusao requisito 9

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,19 +1,13 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-interface Fighter {
-  lifePoints: number;
-  strength: number;
+interface Fighter extends SimpleFighter{
   defense: number;
   energy?: Energy;
-
-  attack(enemy: Fighter): void;
 
   special?(enemy: Fighter): void;
 
   levelUp(): void;
-
-  receiveDamage(attackPoints: number): number;
-
 }
 
 export default Fighter;
