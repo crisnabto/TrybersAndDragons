65729899ed8b20b666233e9a6510e68c2895741a
Crisna Bezerra <crisnabto@gmail.com>
2022-11-21 16:38:00 -0300

    Requisito 11

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,26 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  private _monsters: Fighter[] | SimpleFighter[];
+
+  constructor(
+    player: Fighter, 
+    monsters: Fighter[] | SimpleFighter[],
+  ) {
+    super(player);
+    this._monsters = monsters;
+  }
+
+  fight(): number {
+    this._monsters.forEach((monster) => {
+      if (monster.lifePoints !== -1 && this.player.lifePoints !== -1) {
+        monster.attack(this.player);
+        this.player.attack(monster);
+      }
+    });
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default PVE;
