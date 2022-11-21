920bcdb4190ee92801a2587c95164afedd5cf0cd
Crisna Bezerra <crisnabto@gmail.com>
2022-11-21 16:18:32 -0300

    Requisito 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,22 @@
+// import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _player2: Fighter;
+
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    if (this.player.lifePoints && this._player2.lifePoints !== -1) {
+      this._player2.attack(this.player);
+      this.player.attack(this._player2);
+    } 
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default PVP;
